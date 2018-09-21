65f844afeb87ae3ce9c375e658bc6608b20e9ec5
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-21 17:00:29 -0400

    init commit

---
+++ CodingClub/src/com/matladyhandsnam/sorting/SelectionSort.java
@@ -0,0 +1,51 @@
+package com.matladyhandsnam.sorting;
+
+public class SelectionSort {
+
+	public static void selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int min = arr[i];
+			int index = i;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[j] < min) {
+					min = arr[j];
+					index = j;
+				}
+
+			}
+			int temp = arr[i];
+			arr[i] = min;
+			arr[index] = temp;
+		}
+		print(arr);
+
+	}
+	
+	private static void insertionSort(int[] arr)
+	{
+		for (int i = 0; i<arr.length; i++) {
+			int temp;
+			for (int j = i; j > 0  && arr[j] < arr[j-1]; j--) {
+				temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+			}
+		}
+		print(arr);
+	}
+
+	private static void print(int[] i) {
+		for (int x : i) {
+			System.out.print(x + ", ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int[] i = { 1, 2, 3, 3, 4, 3, 2, 66, 5 };
+		insertionSort(i);
+	}
+
+}
+
+
