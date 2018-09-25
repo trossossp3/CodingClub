35516feed48f54968dbe995f836e4813b4373c5a
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2018-09-25 09:00:07 -0400

    did some debugging

--- CodingClub/src/com/matladyhandsnam/sorting/BogoSort.java
+++ CodingClub/src/com/matladyhandsnam/sorting/BogoSort.java
@@ -8,6 +8,9 @@ public class BogoSort {
 		while (!isSorted(i)) {
 			shuffle(i);
 			counter++;
+			/*
+			 * if(counter%1000000==0) { System.out.println(counter); }
+			 */
 		}
 		System.out.println("Solution found! (shuffled " + counter + " times)");
 		print(i);
@@ -22,7 +25,8 @@ public class BogoSort {
 
 	private void shuffle(int[] i) {
 		for (int x = 0; x < i.length; ++x) {
-			int index1 = (int) (Math.random() * i.length), index2 = (int) (Math.random() * i.length);
+			int index1 = (int) (Math.random() * i.length);
+			int index2 = (int) (Math.random() * i.length);
 			int a = i[index1];
 			i[index1] = i[index2];
 			i[index2] = a;
@@ -39,7 +43,7 @@ public class BogoSort {
 	}
 
 	public static void main(String[] args) {
-		int[] i = { 1, 5, 2, 8, 5, 4, 1, 5, 7, 88, 98, 24, 4, 212, 32, 1, 312, 132 };
+		int[] i = { 237642, 962984, 510246, 327116, 233596, 69512, 696262, 698020, 334453, 283565, };
 		new BogoSort(i);
 	}
 
---
+++ CodingClub/src/com/matladyhandsnam/sorting/RandomNums.java
@@ -0,0 +1,42 @@
+package com.matladyhandsnam.sorting;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class RandomNums {
+
+	private static void random(int n1, int n2, int n3) throws IOException {
+		int[] arr = new int[n3];
+		for (int i = 0; i < n3; i++) {
+			if (n1 == 0)
+				arr[i] = ((int) (Math.random() * (n2 + 1)));
+			else {
+				arr[i] = ((int) (Math.random() * ((n2 - n1) + 1) + n1));
+			}
+
+		}
+		print(arr);
+	}
+
+	private static void print(int[] arr) throws IOException {
+
+		FileWriter fileWriter = new FileWriter(
+				"C:\\Dev\\CodingClub\\CodingClub\\src\\com\\matladyhandsnam\\sorting\\test1.txt");
+		PrintWriter printWriter = new PrintWriter(fileWriter);
+
+		for (int i = 0; i < arr.length; i++) {
+			printWriter.write(arr[i] + ",");
+		}
+		printWriter.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		int lowerBound = -100;
+		int upperBound = 1001000;
+		int amountNums = 10;
+		random(lowerBound, upperBound, amountNums);
+	}
+
+}
